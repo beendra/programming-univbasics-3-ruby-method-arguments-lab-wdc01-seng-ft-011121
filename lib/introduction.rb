e67a6b87a5662eae8dcac5cc46dbf2fcab01206b
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-10 01:57:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,11 @@ end
 
 introduction_with_language("Alex", "Ruby")
 
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language_optional()
 
 
 
